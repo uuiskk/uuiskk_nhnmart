6e9e78b7c4810f1278ed7f4b2d4bcabad9076431
nhn-academy-marco <marco@ nhnacademy.com>
2024-09-03 18:44:16 +0900

    step5 - exercise 추가

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -7,14 +7,13 @@ public class App {
     public static void main(String[] args) {
 
         //TODO#5-1 capacity를 100으로 enteringQueue를 초기화 합니다.
-        EnteringQueue enteringQueue = new EnteringQueue(100);
+        EnteringQueue enteringQueue = null;
 
         //TODO#5-2 customerGenerator를 이용해서 thread를 생성 합니다.
-        CustomerGenerator customerGenerator = new CustomerGenerator(enteringQueue);
-        Thread enteringThread = new Thread(customerGenerator);
+        CustomerGenerator customerGenerator = null;
+        Thread enteringThread = null;
 
         //TODO#5-3 enteringThread의 이름을 'entering-thread'로 설정, enteringThread를 시작 합니다.
-        enteringThread.setName("entering-thread");
-        enteringThread.start();
+
     }
 }
