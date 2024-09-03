2b2f8c7a1326ce45486a0d636ee3f42894c0b1ab
nhn-academy-marco <marco@ nhnacademy.com>
2024-09-03 13:29:09 +0900

    step3-enteringQueue예제 추가

--- src/main/java/com/nhnacademy/customer/cart/CartItem.java
+++ src/main/java/com/nhnacademy/customer/cart/CartItem.java
@@ -11,27 +11,27 @@ public class CartItem implements Serializable {
     private final int quantity;
 
     public CartItem(long productId, int quantity) {
-        //TODO#2-6 productId < 0 or quantity<0 IllegalArgumentException이 발생 합니다.
+        //productId < 0 or quantity<0 IllegalArgumentException이 발생 합니다.
         if(productId<0 || quantity <0){
             throw new IllegalArgumentException("productId > 0");
         }
 
-        //TODO#2-7 productId, quantity 를 초기화 합니다.
+        //productId, quantity 를 초기화 합니다.
         this.productId = productId;
         this.quantity = quantity;
     }
 
     public long getProductId() {
-        //TODO#2-8 productId 반환 합니다.
+        //productId 반환 합니다.
         return productId;
     }
 
     public int getQuantity() {
-        //TODO#2-9 quantity를 반환 합니다.
+        //quantity를 반환 합니다.
         return quantity;
     }
 
-    //TODO#2-10  (projectId, quantity)를 기준으로 객체 비교를 하기 위해서 equals() 구현
+    //(projectId, quantity)를 기준으로 객체 비교를 하기 위해서 equals() 구현
 
     @Override
     public boolean equals(Object o) {
@@ -41,7 +41,7 @@ public class CartItem implements Serializable {
         return productId == cartItem.productId && quantity == cartItem.quantity;
     }
 
-    //TODO#2-11 (projectId, quantity)를 기준으로 hashCode() 구현
+    //(projectId, quantity)를 기준으로 hashCode() 구현
     @Override
     public int hashCode() {
         return Objects.hash(productId, quantity);
---
+++ src/test/java/com/nhnacademy/customer/nhnmart/entring/EnteringQueueTest.java
@@ -0,0 +1,96 @@
+package com.nhnacademy.customer.nhnmart.entring;
+
+import com.nhnacademy.customer.domain.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.junit.platform.commons.function.Try;
+import org.junit.platform.commons.util.ReflectionUtils;
+
+@Slf4j
+class EnteringQueueTest {
+    EnteringQueue enteringQueue;
+    @BeforeEach
+    void setUp() {
+        enteringQueue = new EnteringQueue();
+
+        /*TODO#3-9
+            Customer{id=1, name='NHN아카데미1', money=1000000}
+            ~
+            Customer{id=99, name='NHN아카데미99', money=1000000}
+            1~99 고객을 생성 후 enteringQueue 대기열에 등록 합니다.
+         */
+
+        for(int i=0; i<99; i++){
+            Customer customer = new Customer(i+1,String.format("NHN아카데미%d", i+1),100_0000);
+            log.debug("{}",customer);
+            enteringQueue.addCustomer(customer);
+        }
+    }
+
+    @Test
+    @DisplayName("default queue capacity = 100")
+    void constructorTest_InitCapacity() throws Exception {
+        Try<Object> capacity = ReflectionUtils.tryToReadFieldValue(EnteringQueue.class,"capacity",enteringQueue);
+        Assertions.assertEquals(100, (int)capacity.get() );
+    }
+
+    @Test
+    void addCustomer() throws Exception {
+        //TODO#3-10 id=100인 고객을 enteringQueue에 등록하고 검증 합니다.
+        Customer customer = new Customer(100l, "NHN아카데미100",100_0000);
+        enteringQueue.addCustomer(customer);
+        int actual = enteringQueue.getQueueSize();
+        Assertions.assertEquals(100, actual);
+    }
+
+    @Test
+    @DisplayName("queue - poll test")
+    void getCustomer() {
+        //TODO#3-11  enteringQueue에서 enteringQueue.getCustomer() 호출시 반환되는 값을 검증 합니다.
+        Customer actual = enteringQueue.getCustomer();
+        Customer excepted = new Customer(1l, "NHN아카데미1",100_0000);
+        Assertions.assertEquals(excepted,actual);
+        log.debug("customer:{}",actual);
+    }
+
+    @Test
+    @DisplayName("blocking queue test : queue size : 100, 101번째 customer를 추가 한다면, consumer에 의해서 소비될 때 까지 대기 합니다.")
+    void blockingTest() throws Exception {
+
+        Thread producer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Customer customer100= new Customer(100l, "NHN아카데미100",100_0000);
+                log.debug("2초 대기 후 101-customer 추가 됨");
+                Customer customer101= new Customer(101l, "NHN아카데미101",100_0000);
+                enteringQueue.addCustomer(customer100);
+                enteringQueue.addCustomer(customer101);
+            }
+        });
+        producer.start();
+
+        Thread consumer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //2초 대기 후 getCustomer()를 호출 합니다.
+                    Thread.sleep(2000);
+                    enteringQueue.getCustomer();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        consumer.start();
+
+        //TODO#3-12  producer or consumer 실행 중이라면 대기 합니다. yield()를 이용해서 구현하세요.
+
+        while (producer.isAlive() || consumer.isAlive()){
+            Thread.yield();
+        }
+
+        int actual = enteringQueue.getQueueSize();
+
+        Assertions.assertEquals(100,actual);
+    }
+}
