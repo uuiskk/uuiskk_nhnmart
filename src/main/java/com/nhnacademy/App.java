adabd4223faee915bde0e850d74a6de812240acf
nhn-academy-marco <marco@ nhnacademy.com>
2024-09-06 20:01:27 +0900

    step10

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -8,6 +8,10 @@ import com.nhnacademy.nhnmart.product.repository.ProductRepository;
 import com.nhnacademy.nhnmart.product.repository.impl.MemoryProductRepository;
 import com.nhnacademy.nhnmart.product.service.ProductService;
 import com.nhnacademy.nhnmart.product.service.impl.ProductServiceImpl;
+import com.nhnacademy.thread.ThreadPool;
+import com.nhnacademy.thread.customer.CustomerShoppingHandler;
+import com.nhnacademy.thread.util.RequestChannel;
+import com.nhnacademy.thread.util.RequestHandler;
 
 public class App
 {
@@ -31,5 +35,41 @@ public class App
         //ProductServiceImpl 구현체를 이용해서 ProductService 객체를 생성 합니다.
         ProductService productService = new ProductServiceImpl(productRepository,productParser);
 
+
+
+        /*
+            쇼핑은 동시에 최대 5명까지 할 수 있습니다.
+            CartStore에서 cart를 대여 한다.
+            cart를 대여 후 쇼핑을 한다.
+            쇼핑이 완료되면 계산을 할 수 있도록 대기 한다.
+        */
+
+        //checkout 대기열의 queueSize : 20으로 설정 합니다.
+        RequestChannel checkoutChannel = new RequestChannel(20);
+
+        //shoppingThreadPool 생성후 실행 합니다.
+        Runnable customerRunnable = new CustomerShoppingHandler(enteringQueue,productService, checkoutChannel);
+        ThreadPool shoppingThreadPool = new ThreadPool(10,customerRunnable);
+        shoppingThreadPool.start();
+
+        //checkout을 하기위한 threadPool을 생성 합니다. poolSize =3 , 즉 동시에 3군대서 계산을 진행할 수 있습니다.
+        RequestHandler requestHandler = new RequestHandler(checkoutChannel);
+        ThreadPool checkOutThreadPool = new ThreadPool(3,requestHandler);
+        //checkOutThreadPool.start();
+
+        // 60초 후 종료 됩니다.
+        try {
+            Thread.sleep(10000);
+            enteringThread.interrupt();
+            enteringThread.join();
+
+            Thread.sleep(2000);
+
+            shoppingThreadPool.stop();
+
+           // checkOutThreadPool.stop();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
